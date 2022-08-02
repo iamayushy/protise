3160689778314bd6b675441bd40c678cd6b2892a
iamayushy <ayushhuns@gmail.com>
2022-08-02 20:28:39 +0530

    implemented a scrollbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,15 @@
 import nav from '../cssmodule/nav.module.css'
 import { catAdapter } from '../categories'
+import { useRef } from 'react'
+import {BiLeftArrow, BiRightArrow} from 'react-icons/bi'
 const Navbar = () => {
+    const eleRef = useRef()
+
+    const move = (val) => {
+        const ele = eleRef.current
+        ele.scrollBy(0+val, 0)
+    }
+
     return(
         <div className={nav.navbar}>
             <section className={nav.layout}>
@@ -12,11 +21,13 @@ const Navbar = () => {
             </section>
             </section>
             <section className={nav.hadapter}>
-                <ul className={nav.flow}>
-                    {catAdapter && catAdapter.map((category) => {
-                        return <li>{category}</li>
+                <BiLeftArrow size={20} className={nav.left} onClick={() => move(250)} />
+                <ul ref={eleRef}  className={nav.flow}>
+                    {catAdapter && catAdapter.map((category, index) => {
+                        return <li key={index} >{category}</li>
                     })}
                 </ul>
+                <BiRightArrow size={20} className={nav.right} onClick={() => move(-250)}/>
             </section>
         </div>
     )
